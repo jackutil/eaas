4ff378a1cbd14b45147ba3c6550e6ad5f8e6966f
jackutil <jakob.reichle00@gmail.com>
2026-03-05 17:25:42 +0100

    init commit migration service

---
+++ aas-migration/src/main/java/dev/jackutil/aasmigration/AasMigrationApplication.java
@@ -0,0 +1,13 @@
+package dev.jackutil.aasmigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AasMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AasMigrationApplication.class, args);
+    }
+
+}
---
+++ aas-migration/src/test/java/dev/jackutil/aasmigration/AasMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jackutil.aasmigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AasMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
